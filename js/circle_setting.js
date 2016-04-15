a5a5fbb91b026735b4a6032ebdaa03020edc52c9
krash4krash <rarirurero.ga.iemasen@gmail.com>
2016-04-15 18:47:43 +0900

    add new file

---
+++ js/circle_setting.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	// WEB DESIGN
+	$('#circle90').one('inview', function() {
+	    $(this).circliful({
+	        percent: 90,
+	        foregroundColor: "#32ADE1"
+	    });
+	});
+	// HTML / CSS
+	$('#circle75').one('inview', function() {
+	    $(this).circliful({
+	        percent: 75,
+	        foregroundColor: "#CF3C77"
+	    });
+	});
+	// GRAPHIC DESIGN
+	$('#circle70').one('inview', function() {
+	    $(this).circliful({
+	        percent: 70,
+	        foregroundColor: "#26BF98"
+	    });
+	});
+	// UI / UX
+	$('#circle85').one('inview', function() {
+	    $(this).circliful({
+	        percent: 85,
+	        foregroundColor: "#E46A3F"
+	    });
+	});
+});
+
+
